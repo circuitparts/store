c58451a5052996390744b900e988e094aa24e75f
deepak-jdr <deepak@circuitparts.in>
2024-02-19 06:01:50 +0530

    updated sitemaps lastmodified date

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,31 +4,31 @@ export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
 			url: "https://circuitparts.in",
-			lastModified: new Date(),
+			lastModified: "2024-02-18T00:29:15.456Z",
 			changeFrequency: "monthly",
 			priority: 1,
 		},
 		{
 			url: "https://circuitparts.in/shipping-and-returns",
-			lastModified: new Date(),
+			lastModified: "2024-02-18T00:29:15.456Z",
 			changeFrequency: "yearly",
 			priority: 0.8,
 		},
 		{
 			url: "https://circuitparts.in/terms-and-conditions",
-			lastModified: new Date(),
+			lastModified: "2024-02-18T00:29:15.456Z",
 			changeFrequency: "yearly",
 			priority: 0.8,
 		},
 		{
 			url: "https://circuitparts.in/privacy-policy",
-			lastModified: new Date(),
+			lastModified: "2024-02-18T00:29:15.456Z",
 			changeFrequency: "yearly",
 			priority: 0.8,
 		},
 		{
 			url: "https://circuitparts.in/contact-us",
-			lastModified: new Date(),
+			lastModified: "2024-02-18T00:29:15.456Z",
 			changeFrequency: "yearly",
 			priority: 0.8,
 		},
