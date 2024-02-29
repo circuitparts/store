3892efe94effa479cec71c93648023ea327a9429
deepak-jdr <deepak@circuitparts.in>
2024-02-29 08:57:12 +0530

    test: optimise auth test cases

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,118 +1,42 @@
-import { test, expect } from "@playwright/test";
-import path from "path";
+import { BASE_URL, addRigidPcbToCart, login, logout, signup } from "@/tests/test-functions";
+import { expect, test } from "@playwright/test";
 
-test("signup flow test", async ({ page }) => {
-	// add a pcb to cart to test cart porting after signup
-	await page.goto("http://localhost:3000/");
-	await page.getByTestId("rigid-pcb-nav-link").click();
+const EMPTY_CART_SIZE = "0";
 
-	// set pcb name and keep remaining fields default
-	const testPcbName = "bulbasaur";
-	await page.getByTestId("rigid-pcb-name").fill(testPcbName);
+// Add a pcb to cart to test cart porting after signup
+test("signup", async ({ page }) => {
+	await page.goto(BASE_URL);
+	const initialCartSize = await page.getByTestId("cart-qty").textContent(); // initial cart size
+	await addRigidPcbToCart({ page, cartSize: initialCartSize });
+	const newCartSize = `${Number(initialCartSize) + 1}`;
+	await expect(page.getByTestId("cart-qty")).toHaveText(newCartSize); // check if the cart size increased
 
-	// wait for the debounce to complete
-	await page.waitForTimeout(2000);
+	await signup({ page });
 
-	// upload design file
-	const fileChooserPromise = page.waitForEvent("filechooser");
-	await page.getByTestId("rigid-pcb-fab-upload-design-file").click();
-	const fileChooser = await fileChooserPromise;
-	await fileChooser.setFiles(path.join(__dirname, "test.zip"));
-	await page.getByTestId("rigid-pcb-fab-upload-design-file-button").click();
-	await expect(page.getByTestId("toast-title")).toHaveText("File upload success");
+	// expect cart size to be the same
+	await expect(page.getByTestId("cart-qty")).toHaveText(newCartSize);
 
-	// wait for price to update
-	await page.waitForTimeout(2000);
-
-	// add to cart
-	const cartSize = await page.getByTestId("cart-qty").textContent();
-	await page.getByTestId("add-to-cart-button").click();
-	await expect(page.getByTestId("toast-title")).toHaveText("Rigid PCB added to cart");
-
-	// check if the cart size increased
-	await expect(page.getByTestId("cart-qty")).toHaveText(`${Number(cartSize) + 1}`);
-
-	// signup
-	await page.goto("http://localhost:3000/auth/signup");
-
-	// fill the signup form with random data
-	const randomNumber = Math.floor(Math.random() * 100000); // to make the fields unique
-	await page.getByTestId("fname-input").fill(`test_first_name_${randomNumber}`);
-	await page.getByTestId("lname-input").fill(`test_last_name_${randomNumber}`);
-	await page.getByTestId("email-input").fill(`test_email_${randomNumber}+clerk_test@domain.com`);
-	await page.getByTestId("password-input").fill("TestPassword@123");
-	await page.getByTestId("create-account-button").click();
-
-	// verify email page
-	await page.getByTestId("verify-email-code-input").fill("424242");
-	await page.getByTestId("verify-email-submit-button").click();
-
-	await page.waitForTimeout(3000); // wait for the page to load
-	await expect(page).toHaveURL("http://localhost:3000/");
-	await page.waitForTimeout(3000); // wait for the cart to load its value
-
-	// check if the cart size is still the same
-	await expect(page.getByTestId("cart-qty")).toHaveText(`${Number(cartSize) + 1}`);
-
-	// logout
-	await page.getByTestId("user-menu-trigger-button").click();
-	await page.getByTestId("logout-button").click();
+	await logout({ page });
 
 	// check if cart is reset
-	await expect(page.getByTestId("cart-qty")).toHaveText("0");
+	await expect(page.getByTestId("cart-qty")).toHaveText(EMPTY_CART_SIZE);
 });
 
-//NOTE: make sure you have a user with the email and password in your database for this test to pass
-test("login flow test", async ({ page }) => {
-	// add a pcb to cart to test cart porting after signup
-	await page.goto("http://localhost:3000/");
-	await page.getByTestId("rigid-pcb-nav-link").click();
+// Add a pcb to cart to test cart porting after login
+test("login", async ({ page }) => {
+	await page.goto(BASE_URL);
+	const initialCartSize = await page.getByTestId("cart-qty").textContent(); // initial cart size
+	await addRigidPcbToCart({ page, cartSize: initialCartSize });
+	const newCartSize = `${Number(initialCartSize) + 1}`;
+	await expect(page.getByTestId("cart-qty")).toHaveText(newCartSize); // check if the cart size increased
 
-	// set pcb name and keep remaining fields default
-	const testPcbName = "bulbasaur";
-	await page.getByTestId("rigid-pcb-name").fill(testPcbName);
+	await login({ page });
 
-	// wait for the debounce to complete
-	await page.waitForTimeout(2000);
+	// expect cart size to be the same
+	await expect(page.getByTestId("cart-qty")).toHaveText(newCartSize);
 
-	// upload design file
-	const fileChooserPromise = page.waitForEvent("filechooser");
-	await page.getByTestId("rigid-pcb-fab-upload-design-file").click();
-	const fileChooser = await fileChooserPromise;
-	await fileChooser.setFiles(path.join(__dirname, "test.zip"));
-	await page.getByTestId("rigid-pcb-fab-upload-design-file-button").click();
-	await expect(page.getByTestId("toast-title")).toHaveText("File upload success");
-
-	// wait for price to update
-	await page.waitForTimeout(2000);
-
-	// add to cart
-	const cartSize = await page.getByTestId("cart-qty").textContent();
-	await page.getByTestId("add-to-cart-button").click();
-	await expect(page.getByTestId("toast-title")).toHaveText("Rigid PCB added to cart");
-
-	// check if the cart size increased
-	await expect(page.getByTestId("cart-qty")).toHaveText(`${Number(cartSize) + 1}`);
-
-	// signup
-	await page.goto("http://localhost:3000/auth/login");
-
-	// fill the login form with test user data
-	await page.getByTestId("email-input").fill("test+clerk_test@test.com");
-	await page.getByTestId("password-input").fill("TestPassword@123");
-	await page.getByTestId("sign-in-button").click();
-
-	await page.waitForTimeout(3000); // wait for the page to load
-	await expect(page).toHaveURL("http://localhost:3000/");
-	await page.waitForTimeout(3000); // wait for the cart to load its value
-
-	// check if the cart size is still the same
-	await expect(page.getByTestId("cart-qty")).toHaveText(`${Number(cartSize) + 1}`);
-
-	// logout
-	await page.getByTestId("user-menu-trigger-button").click();
-	await page.getByTestId("logout-button").click();
+	await logout({ page });
 
 	// check if cart is reset
-	await expect(page.getByTestId("cart-qty")).toHaveText("0");
+	await expect(page.getByTestId("cart-qty")).toHaveText(EMPTY_CART_SIZE);
 });
--- tests/test-functions.ts
+++ tests/test-functions.ts
@@ -1,14 +1,16 @@
 import {
 	FLEX_PCB_FAB_PAGE,
+	LOGIN_PAGE,
 	PART_DETAILS_PAGE,
 	PART_RESULTS_PAGE,
 	PCB_ASSEMBLY_PAGE,
 	RIGID_PCB_FAB_PAGE,
+	SIGNUP_PAGE,
 } from "@/lib/constants/page-routes";
 import { expect, type Page } from "@playwright/test";
 import path from "path";
 
-const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "http://localhost:3000";
 
 export async function searchComponent(props: { partNumber: string; page: Page }) {
 	const { partNumber, page } = props;
@@ -99,4 +101,111 @@ export async function uploadPcbDesignFile(page: Page) {
 	await fileChooser.setFiles(path.join(__dirname, "test.zip"));
 	await page.getByTestId("upload-pcb-design-file-button").click();
 	await expect(page.getByTestId("toast-title")).toHaveText("File upload success");
+}
+
+export async function addRigidPcbToCart(props: { page: Page; cartSize: string | null }) {
+	const { page, cartSize } = props;
+	await navigateToPcbPage({ page, pcbType: "rigid" });
+	await setPcbNameAndQuantity({
+		pcbType: "rigid",
+		pcbName: "charmander",
+		quantity_1: 5, // initial quantity
+		quantity_2: 10, // new quantity
+		expectedTotal: "₹5,258.88", // expected total after changing quantity
+		page,
+	});
+	await uploadPcbDesignFile(page);
+
+	// add to cart
+	await page.getByTestId("add-to-cart-button").click();
+	await expect(page.getByTestId("toast-title")).toHaveText("Rigid PCB added to cart");
+
+	// check if the cart size increased
+	await expect(page.getByTestId("cart-qty")).toHaveText(`${Number(cartSize) + 1}`);
+}
+
+export async function addFlexPcbToCart(props: { page: Page; cartSize: string | null }) {
+	const { page, cartSize } = props;
+	await navigateToPcbPage({ page, pcbType: "flex" });
+	await setPcbNameAndQuantity({
+		pcbType: "flex",
+		pcbName: "charmeleon",
+		quantity_1: 5, // initial quantity
+		quantity_2: 10, // new quantity
+		expectedTotal: "₹2,357.20", // expected total after changing quantity
+		page,
+	});
+	await uploadPcbDesignFile(page);
+
+	// add to cart
+	await page.getByTestId("add-to-cart-button").click();
+	await expect(page.getByTestId("toast-title")).toHaveText("Flex PCB added to cart");
+
+	// check if the cart size increased
+	await expect(page.getByTestId("cart-qty")).toHaveText(`${Number(cartSize) + 1}`);
+}
+
+export async function addPcbAssemblyToCart(props: { page: Page; cartSize: string | null }) {
+	const { page, cartSize } = props;
+	await navigateToPcbPage({ page, pcbType: "assembly" });
+	await setPcbNameAndQuantity({
+		pcbType: "assembly",
+		pcbName: "charizard",
+		quantity_1: 10, // initial quantity
+		quantity_2: 15, // new quantity
+		expectedTotal: "₹10,831.50", // expected total after changing quantity
+		page,
+	});
+	await uploadPcbDesignFile(page);
+
+	// add to cart
+	await page.getByTestId("add-to-cart-button").click();
+	await expect(page.getByTestId("toast-title")).toHaveText("PCB Assembly added to cart");
+
+	// check if the cart size increased
+	await expect(page.getByTestId("cart-qty")).toHaveText(`${Number(cartSize) + 1}`);
+}
+
+export async function signup(props: { page: Page }) {
+	const { page } = props;
+
+	await page.goto(BASE_URL + SIGNUP_PAGE);
+
+	// fill the signup form with random data
+	const randomNumber = Math.floor(Math.random() * 100000); // to make the fields unique
+	await page.getByTestId("fname-input").fill(`test_first_name_${randomNumber}`);
+	await page.getByTestId("lname-input").fill(`test_last_name_${randomNumber}`);
+	await page.getByTestId("email-input").fill(`test_email_${randomNumber}+clerk_test@domain.com`);
+	await page.getByTestId("password-input").fill("TestPassword@123");
+	await page.getByTestId("create-account-button").click();
+
+	// verify email page
+	await page.getByTestId("verify-email-code-input").fill("424242");
+	await page.getByTestId("verify-email-submit-button").click();
+
+	await page.waitForTimeout(3000); // wait for the page to load
+	await expect(page).toHaveURL(BASE_URL);
+	await page.waitForTimeout(3000); // wait for the cart to load its value
+}
+
+//NOTE: make sure you have a user with the email and password in your database for this test to pass
+export async function login(props: { page: Page }) {
+	const { page } = props;
+
+	await page.goto(BASE_URL + LOGIN_PAGE);
+
+	// fill the login form with test user data
+	await page.getByTestId("email-input").fill("test+clerk_test@test.com");
+	await page.getByTestId("password-input").fill("TestPassword@123");
+	await page.getByTestId("sign-in-button").click();
+
+	await page.waitForTimeout(3000); // wait for the page to load
+	await expect(page).toHaveURL(BASE_URL);
+	await page.waitForTimeout(3000); // wait for the cart to load its value
+}
+
+export async function logout(props: { page: Page }) {
+	const { page } = props;
+	await page.getByTestId("user-menu-trigger-button").click();
+	await page.getByTestId("logout-button").click();
 }
